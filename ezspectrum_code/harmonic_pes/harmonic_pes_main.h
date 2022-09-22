e7a87cf5bdfb611e46d49921806c6edf828b270b
Pawel Wojcik <pawel.wojcik@usc.edu>
2022-09-22 14:21:11 -0700

    Clean old comments

--- ezspectrum_code/harmonic_pes/harmonic_pes_main.h
+++ ezspectrum_code/harmonic_pes/harmonic_pes_main.h
@@ -5,21 +5,6 @@
   \brief  the "main" for analytic-harmonic Franck-Condon Factors calculation
   */
 
-
-//////////////////////////////////////////////////////////////////////////////
-//                                                                          // 
-//  calculates photoelectron spectrum in 1D Franck-Condon approximation      // 
-//                                                                          // 
-//////////////////////////////////////////////////////////////////////////////
-
-//2DO: 
-// 1 ADD DEGENERATE NORMAL MODES
-// 2 several target states
-// cut into nice picies
-
-//! .xml file name with atomName<->atomMass table
-#define ATOMIC_MASSES_FILE ("atomicMasses.xml")
-
 #include "genincludes.h"
 #include <vector>
 #include <queue>
@@ -33,9 +18,9 @@
 
 #include <limits>
 
-//! program itself: 
-bool harmonic_pes_main (const char *InputFileName, xml_node& node_input, xml_node& node_amu_table);
-
+//! program itself:
+bool harmonic_pes_main(const char *InputFileName, xml_node &node_input,
+                       xml_node &node_amu_table);
 
 #endif
 
